8de24ad2e7da2c3a57ca384cbca3e81d0ec47078
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-11-21 19:09:10 +0100

    send messages for new rss items

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import LastImportTracker from './LastImportTracker'
 import path from 'path'
 import fs from 'fs'
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client, Events, GatewayIntentBits, type SendableChannels } from 'discord.js'
 
 const configPath = path.resolve(__dirname, '../config.json')
 const configJson = JSON.parse(fs.readFileSync(configPath, { encoding: 'utf8' }))
@@ -15,24 +15,41 @@ const configSchema = z.object({
     idKey: z.string().min(1)
   })),
   interval: z.number().min(30).max(1440).default(600),
-  token: z.string().length(72)
+  token: z.string().length(72),
+  rssChannels: z.array(
+    z.string().min(0)
+  )
 })
 
 const config = configSchema.parse(configJson)
-
 const parser = new Parser()
-
 const tracker = new LastImportTracker()
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const channels: SendableChannels[] = []
 
 client.once(Events.ClientReady, readyClient => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+
+  for (const channelId of config.rssChannels) {
+    const channel = readyClient.channels.cache.get(channelId)
+
+    if (channel == null) {
+      console.warn(`Channel with id "${channelId}" not found.`)
+      continue
+    }
+    if (!channel.isSendable()) {
+      console.warn(`Channel with id "${channelId}" not sendable.`)
+      continue
+    }
+
+    channels.push(channel)
+  }
+
+  doIt()
+  setInterval(doIt, config.interval * 1000)
 })
 
-client.login(config.token)
-  .then(val => console.log(val))
-  .catch(err => console.error(err))
+void client.login(config.token)
 
 function doIt (): void {
   Promise.all(config.feeds.map(async feedConf => {
@@ -47,21 +64,23 @@ function doIt (): void {
     let idx = 0
     let item = feed.items[0]
     let iterate = lastImported !== null && idx < feed.items.length && item[feedConf.idKey] !== lastImported
-
-    console.log(item)
+    const messages: string[] = []
 
     while (iterate) {
-      console.log(`[${feedConf.title}] ${item.title ?? ''}<br>${item.content ?? ''}<br>${item.link ?? ''}`)
+      messages.push(`**[${feedConf.title}] ${item.title ?? ''}**  \n${item.content ?? ''}  \n${item.link ?? ''}`)
 
       item = feed.items[++idx]
       iterate = idx < feed.items.length && item[feedConf.idKey] !== lastImported
     }
 
     void tracker.setLastImport(feedConf.url, item[feedConf.idKey])
+
+    await Promise.all(
+      messages.map(message =>
+        channels.map(async channel => await channel.send(message))
+      ).flat()
+    )
   })).catch(err => {
     console.log(err)
   })
 }
-
-doIt()
-setInterval(doIt, config.interval * 1000)
